6df211332df27a376a95684ab56ac8be3a9d7645
Adam Fuentes <66355715+Adam-Fuentes@users.noreply.github.com>
2020-12-28 10:15:04 +0100

    Initial commit

---
+++ CSharpOOP/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpOOP
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Warrior theGoodGuy = new Warrior(190, 80, "Good guy");
+
+            Warrior theBadGuy = new Warrior(170, 90, "Bad guy");
+
+            theGoodGuy.Greetings(theBadGuy);
+            theBadGuy.Greetings(theGoodGuy);
+        }
+    }
+}
